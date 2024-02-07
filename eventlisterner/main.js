07dc9cf95d5f8f91d722c1a9885f4f164e8bd1b6
Lemuel Gempesao <lemuelgempesao40@gmail.com>
2024-02-07 10:51:16 +0800

    added

--- eventlisterner/main.js
+++ eventlisterner/main.js
@@ -1,10 +1,11 @@
-const object=document.querySelector(".object");
+const object=document.getElementById("object");
 let x=0;
 let y=0;
-let move=10;
+const move=10;
 
-object.addEventListener("keyup", event=>{
-    if (event.key.startsWith("Arrow");
+document.addEventListener("keydown", event=>{
+
+    if (event.key.startsWith("Arrow")){
 
     switch(event.key){
 
@@ -25,23 +26,24 @@ object.addEventListener("keyup", event=>{
              x+=move;
              break;
     }
-})
+}
 
+    object.style.top=`${y}px`;
+    object.style.left=`${x}px`;
 
-
-
-
-
-
-
-
-
-
-
-const guess=document.querySelector(".guess");
-
-guess.addEventListener("keyup", function (event) {
-    console.log("Key released:", event.key);
-
-    // Your code to handle the key release goes here
 });
+
+
+
+
+
+
+
+
+// const guess=document.querySelector(".guess");
+
+// guess.addEventListener("keyup", function (event) {
+//     console.log("Key released:", event.key);
+
+//     // Your code to handle the key release goes here
+// });
